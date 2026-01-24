ad88b79c7a3a34b9950bf4069577cd1e55093286
agenorpestana <139382276+agenorpestana@users.noreply.github.com>
2026-01-23 21:46:39 -0300

    Update server.js

--- server.js
+++ server.js
@@ -365,6 +365,13 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
+const PORT = process.env.PORT || 3002;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
